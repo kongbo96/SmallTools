14ed13b8af804692f9c8d286beff7ce46a3e4553
Kong Bo <42367656+kongbo96@users.noreply.github.com>
2020-10-24 17:43:24 +0800

    Add files via upload
    筛选出文件名纯数字的或不含中文pdf的进行修改文件名，原文件名添加到标题后边作为新文件名。

--- PdfNameChanger.py
+++ PdfNameChanger.py
@@ -2,17 +2,45 @@ import glob,os
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 import traceback
-files = glob.glob(".//*.pdf")
-for file in files:
-    try:
-        with open(file, 'rb') as fp:
-            parser = PDFParser(fp)
-            doc = PDFDocument(parser)
-        title = doc.info[0]['Title'].decode('utf-8')
-        title = title.replace(':', '：')
-        print(title)
-        os.rename(file, title + ".pdf")
-    except:
-        print(file)
-        traceback.print_exc()
-    print()
+import re
+
+def is_chinese(string):
+    """
+    检查整个字符串是否包含中文
+    :param string: 需要检查的字符串
+    :return: bool
+    """
+    for ch in string:
+        if u'\u4e00' <= ch <= u'\u9fff':
+            return True
+
+    return False
+def is_number(num):
+    #判断是否纯数字
+    pattern = re.compile(r'^[-+]?[-0-9]\d*\.\d*|[-+]?\.?[0-9]\d*$')
+    result = pattern.match(num)
+    if result:
+        return True
+    else:
+        return False
+
+for base,dir,files in os.walk('./'):
+    for file in files:
+        if file[-3:].lower() == 'pdf' and (len(file) <= 20 and not is_chinese(file) or is_number(file[:-3])):
+            file_path = base + '/' + file
+            try:
+                with open(file_path, 'rb') as fp:
+                    parser = PDFParser(fp)
+                    doc = PDFDocument(parser)
+                title = doc.info[0]['Title'].decode('utf-8',errors='ignore')
+                title = title.replace(':', '：')
+                title = title.replace('\00','')
+                title = title + '_' + file
+                if len(title) >= 20:
+                    title_path = base + '/' + title
+                    print('Accomplished:',title_path)
+                    os.rename(file_path,title_path)
+            except:
+                print('Failed:',file_path)
+                traceback.print_exc()
+            print()
